e98c31e491d515d1cdc4fa7d13ab1c2d50409e25
neural-anarchist <garyxhshen@gmail.com>
2025-06-07 19:41:40 -0400

    Fix syntax error

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -1,12 +1,11 @@
-
-
-require 'jeykll-watch'
 module Jekyll
-    module Watcher
-        extend self
-        alias_method : original_listen_ignore_paths, :listen_ignore_paths
-        def listen_ignore_paths(options)
-            original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
-        end
+  module Watcher
+    extend self
+
+    alias_method :original_listen_ignore_paths, :listen_ignore_paths
+
+    def listen_ignore_paths(config)
+      original_listen_ignore_paths(config) + [%r{^_drafts/}]
     end
+  end
 end
